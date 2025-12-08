9477658a8bdd04b64f828daf55b1fd34e947c5c7
ayushmanshukla362 <ayushmanshukla94@gmail.com>
2025-12-08 11:04:09 +0530

    loops on arrays

---
+++ arrays/loopsonarrays.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks [5]={99,100,102,44,34};
+    int size=5;
+    for (int i=0;i<size;i++){
+        cout<<marks[i]<<endl;
+    }
+    return 0;
+}
+//will print all numbers from index 0 to size-1
