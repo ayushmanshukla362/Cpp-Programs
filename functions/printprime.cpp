8a9e52c8d705350369cce96443a2b356fad33af9
ayushmanshukla362 <ayushmanshukla94@gmail.com>
2025-12-28 16:12:39 +0530

    practicing

--- functions/printprime.cpp
+++ functions/printprime.cpp
@@ -21,5 +21,5 @@ int main() {
     int N;
     cin >> N;
     printPrimes(N);
-    return 0;
+    return 0;  
 }
--- practice/minofarr.cpp
+++ practice/minofarr.cpp
@@ -2,20 +2,18 @@
 using namespace std;
 
 int main() {
-    int n;
-     cout<<"enter the number of array elements ";
+    int n,even=0,odd=0;
+    cout<<"enter the number of array elements ";
     cin>>n;
     int arr[n];
-    cout<< "enter the elements of the array ";
-    for (int i=0;i<n;i++){
+    cout<<"enter the elements of array";
+    for(int i=0;i<n;i++){
         cin>>arr[i];
-    }
-    int min= arr[0];
-    for (int i=1;i<n;i++){
-        if(arr[i]<min){
-            min=arr[i];
+        if (arr[i]%2==0)
+            even++;
+            else
+            odd++;
         }
-    }
-    cout<<"the smallest element of the array is "<<min;
+        cout<<even<<"  "<<odd;
     return 0;
 }
