79ec75f32d9e79055e02c454e092423f2fbf9a5a
ayushmanshukla362 <ayushmanshukla94@gmail.com>
2025-12-28 17:42:52 +0530

    searching array

---
+++ practice/searchanarray.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int x;
+    cin >> x;
+
+    bool found = false;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == x) {
+            found = true;
+            break;
+        }
+    }
+
+    if (found)
+        cout << "found";
+    else
+        cout << "not found";
+
+    return 0;
+}
