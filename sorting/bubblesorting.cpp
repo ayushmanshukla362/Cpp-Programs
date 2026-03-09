7c86552b4af5fe5e91fe034705978c296dbb1241
ayushmanshukla362 <ayushmanshukla94@gmail.com>
2026-03-09 12:14:12 +0530

    bubble sorting

---
+++ sorting/bubblesorting.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int arr[] = {5,1,4,2};
+    int n = 4;
+
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+}
