8cd209a135f084ac9a8854989acc686b8703f138
ayushmanshukla362 <ayushmanshukla94@gmail.com>
2026-03-09 12:37:24 +0530

    bubble sorting

--- sorting/bubblesorting.cpp
+++ sorting/bubblesorting.cpp
@@ -2,18 +2,27 @@
 using namespace std;
 
 int main() {
-    int arr[] = {5,1,4,2};
-    int n = 4;
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
 
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){
+    int arr[n];
+
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < n-1; i++){
+        for(int j = 0; j < n-i-1; j++){
             if(arr[j] > arr[j+1]){
                 swap(arr[j], arr[j+1]);
             }
         }
     }
 
-    for(int i=0;i<n;i++){
+    cout << "Sorted array: ";
+    for(int i = 0; i < n; i++){
         cout << arr[i] << " ";
     }
 }
